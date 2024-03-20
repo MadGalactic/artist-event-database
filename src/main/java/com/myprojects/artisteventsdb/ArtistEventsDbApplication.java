a25f6209030e0f29bc3c4a565cb0fac3dbd85a56
MVDLNE <madelinelora95@icloud.com>
2024-03-19 22:40:04 -0400

    Initialization of project

---
+++ src/main/java/com/myprojects/artisteventsdb/ArtistEventsDbApplication.java
@@ -0,0 +1,13 @@
+package com.myprojects.artisteventsdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtistEventsDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtistEventsDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myprojects/artisteventsdb/ArtistEventsDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myprojects.artisteventsdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtistEventsDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
